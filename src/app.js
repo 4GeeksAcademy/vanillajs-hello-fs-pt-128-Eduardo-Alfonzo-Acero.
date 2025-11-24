0463f0463a95ab43a95e0fd5a11b962cac698e59
Edu100625 <eduardojacero@gmail.com>
2025-11-24 20:10:25 +0000

    generador completo

--- src/app.js
+++ src/app.js
@@ -1,19 +1,20 @@
- let pronoun = ['the', 'our'];
-  let adj = ['great', 'big'];
-  let noun = ['jogger', 'racoon'];
+let pronoun = ['the', 'our', 'the'];
+let adj = ['great', 'big', 'last'];
+let noun = ['jogger', 'racoon', 'ofus'];
+let extenciones = ['com', 'net', 'us', 'io', 'es']
 
-  for (let i = 0; i < pronoun.length; i++) {
+for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
-      for (let k = 0; k < noun.length; k++) {
-        console.log(`${pronoun[i]}${adj[j]}${noun[k]}.com`);
-
-        
-        
-        
-      }
-      
+        for (let k = 0; k < noun.length; k++) {
+            for (let l = 0; l < extenciones.length; l++) {
+                let dominio = pronoun[i]+adj[j]+noun[k]
+                if (dominio.endsWith(extenciones[l])) {
+                    console.log(dominio.slice(0,(dominio.length - extenciones[l].length)) + "." + extenciones[l])
+                } else {
+                    console.log(dominio + "." + extenciones[l])
+                }
+            }
+        }
     }
-    
-    
-  }
+}
   
